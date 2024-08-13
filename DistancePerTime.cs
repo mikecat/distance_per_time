0aaa0f6286f5bab4b29fe05f39c4d1c9661029f8
MikeCAT <mikecat@users.noreply.github.com>
2024-08-13 22:45:28 +0900

    implement calculation

--- DistancePerTime.cs
+++ DistancePerTime.cs
@@ -102,5 +102,113 @@ class DistancePerTime: Form
 		timingPredictionLabel.TextAlign = ContentAlignment.MiddleRight;
 
 		ResumeLayout();
+
+		Load += LoadHandler;
+		FormClosed += FormClosedHandler;
+	}
+
+	private Timer timer = null;
+	private bool trainCrewEnabled = false;
+	private int stationDataRequestPrevent = 0;
+
+	private void LoadHandler(object sender, EventArgs e)
+	{
+		TrainCrewInput.Init();
+		TrainCrewInput.RequestStaData();
+		trainCrewEnabled = true;
+		timer = new Timer();
+		timer.Interval = 15;
+		timer.Tick += TickHandler;
+		timer.Start();
+	}
+
+	private void FormClosedHandler(object sender, EventArgs e)
+	{
+		trainCrewEnabled = false;
+		TrainCrewInput.Dispose();
+		if (timer != null) timer.Stop();
+	}
+
+	private void TickHandler(object sender, EventArgs e)
+	{
+		if (!trainCrewEnabled) return;
+		TrainState trainState = TrainCrewInput.GetTrainState();
+		if (stationDataRequestPrevent <= 0)
+		{
+			if (trainState.stationList.Count == 0)
+			{
+				TrainCrewInput.RequestStaData();
+				stationDataRequestPrevent = 10;
+			}
+		}
+		else
+		{
+			stationDataRequestPrevent--;
+		}
+		distanceLabel.Text = string.Format("{0} m", Math.Truncate(trainState.nextUIDistance));
+		currentSpeedLabel.Text = string.Format("{0:0.0} km/h", trainState.Speed);
+		float distance = trainState.nextUIDistance;
+		TimeSpan? timeToArrive = null;
+		if (trainState.nowStaIndex < trainState.stationList.Count)
+		{
+			int minIndex = trainState.nowStaIndex;
+			float minScore = Math.Abs(trainState.nextUIDistance - trainState.nextStaDistance);
+			for (int i = trainState.nowStaIndex + 1; i < trainState.stationList.Count; i++)
+			{
+				float thisStaDistance =
+					trainState.nextStaDistance +
+					trainState.stationList[i].TotalLength -
+					trainState.stationList[trainState.nowStaIndex].TotalLength;
+				float score = Math.Abs(trainState.nextUIDistance - thisStaDistance);
+				if (score < minScore)
+				{
+					minIndex = i;
+					minScore = score;
+				}
+			}
+			timeToArrive = trainState.stationList[minIndex].ArvTime;
+		}
+		if (timeToArrive.HasValue)
+		{
+			double time = timeToArrive.Value.TotalSeconds - trainState.NowTime.TotalSeconds;
+			timeLabel.Text = string.Format("{0} 秒", Math.Ceiling(time));
+			double distancePerTime = time <= 0 ? Double.PositiveInfinity : distance / time;
+			if (distancePerTime >= 1000)
+			{
+				distancePerTimeLabel.Text = "∞ m/s";
+				kmPerHourLabel.Text = "∞ km/h";
+			}
+			else
+			{
+				distancePerTimeLabel.Text = string.Format("{0:0.0} m/s", distancePerTime);
+				kmPerHourLabel.Text = string.Format("{0:0.0} km/h", distancePerTime * 3.6);
+			}
+			double estimatedTimeToArrive = trainState.Speed <= 0 ? Double.PositiveInfinity : distance / (trainState.Speed / 3.6);
+			double estimatedTimeDifference = trainState.NowTime.TotalSeconds + estimatedTimeToArrive - timeToArrive.Value.TotalSeconds;
+			double etdSeconds = Math.Floor(estimatedTimeDifference);
+			if (etdSeconds >= 1000)
+			{
+				timingPredictionLabel.Text = "∞ 秒延";
+			}
+			else if (etdSeconds >= 1)
+			{
+				timingPredictionLabel.Text = string.Format("{0} 秒延", etdSeconds);
+			}
+			else if (etdSeconds <= -1)
+			{
+				timingPredictionLabel.Text = string.Format("{0} 秒早", -etdSeconds);
+			}
+			else
+			{
+				timingPredictionLabel.Text = "定時";
+			}
+		}
+		else
+		{
+			timeLabel.Text = "#### 秒";
+			distancePerTimeLabel.Text = "##.# m/s";
+			kmPerHourLabel.Text = "###.# km/h";
+			timingPredictionLabel.Text = "### 秒延";
+		}
 	}
 }
